0bd231668617237fdfa31f70b165ba6f4366ff28
MuzGeek <emamuzo.okerri@gmail.com>
2023-10-21 13:40:14 +0100

    repo init

---
+++ logic.js
@@ -0,0 +1,11 @@
+// DOM elements
+const timeContainer = document.querySelector(".time");
+
+// Global variables
+const currTime = new Date();
+
+// Functions
+const time = function (t) {
+	const locale = navigator.language;
+	const intlOptions = {};
+};
